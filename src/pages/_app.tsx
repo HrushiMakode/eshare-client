906edafe0fc839eb8224b4821746f65def023c49
Hrushikesh Makode <makodehrushikesh@gmail.com>
2021-10-19 21:58:54 +0530

    api base url added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,16 @@ import axios from "axios";
 import "tailwindcss/tailwind.css";
 import "../../styles/globals.css";
 
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <div className="h-screen font-serif  bg-gray-900 text-white grid place-items-center">
-            <div>
-                <Component {...pageProps} />
-            </div>
-        </div>
-    );
+	return (
+		<div className="h-screen font-serif  bg-gray-900 text-white grid place-items-center">
+			<div>
+				<Component {...pageProps} />
+			</div>
+		</div>
+	);
 }
 
 export default MyApp;
--- src/pages/download/[id]/index.tsx
+++ src/pages/download/[id]/index.tsx
@@ -6,70 +6,65 @@ import FileDetails from "@components/FileDetails";
 import fileDownload from "js-file-download";
 
 const index: NextPage<{
-    file: IFile | null;
+	file: IFile | null;
 }> = ({ file }) => {
-    const handleDownload = async () => {
-        const { data } = await axios.get<Blob>(
-            `http://localhost:8080/api/files/${id}/download`,
-            {
-                responseType: "blob",
-            }
-        );
-        fileDownload(data, name);
-    };
+	const handleDownload = async () => {
+		const { data } = await axios.get<Blob>(`/api/files/${id}/download`, {
+			responseType: "blob",
+		});
+		fileDownload(data, name);
+	};
 
-    if (!file)
-        return (
-            <div>
-                <div className="text-center">😞😞😞😞</div>
-                <div>oops! File does not exists! </div>
-                <div>Check the URL again !! 😥😥</div>
-            </div>
-        );
+	if (!file)
+		return (
+			<div>
+				<div className="text-center">😞😞😞😞</div>
+				<div>oops! File does not exists! </div>
+				<div>Check the URL again !! 😥😥</div>
+			</div>
+		);
 
-    const { name, id } = file;
-    return (
-        <div className="flex flex-col p-4 items-center justify-center space-y-4 bg-gray-800 rounded-md shadow-xl">
-            {
-                <>
-                    <img
-                        className="w-16 h-16 "
-                        src="/images/file-download.png"
-                        alt="file-download"
-                    />
-                    <h1 className="text-xl">
-                        Your file is ready to be downloaded
-                    </h1>
-                    <FileDetails file={file} />
-                    <button className="button" onClick={handleDownload}>
-                        📥 Download
-                    </button>
-                </>
-            }
-        </div>
-    );
+	const { name, id } = file;
+	return (
+		<div className="flex flex-col p-4 items-center justify-center space-y-4 bg-gray-800 rounded-md shadow-xl">
+			{
+				<>
+					<img
+						className="w-16 h-16 "
+						src="/images/file-download.png"
+						alt="file-download"
+					/>
+					<h1 className="text-xl">Your file is ready to be downloaded</h1>
+					<FileDetails file={file} />
+					<button className="button" onClick={handleDownload}>
+						📥 Download
+					</button>
+				</>
+			}
+		</div>
+	);
 };
 
 export default index;
 
 export const getServerSideProps = async (
-    context: GetServerSidePropsContext
+	context: GetServerSidePropsContext
 ) => {
-    const { id } = context.query;
-    let file: IFile | null;
-    try {
-        const { data } = await axios.get<IFile>(
-            `http://localhost:8080/api/files/${id}`
-        );
-        file = data;
-    } catch (error) {
-        console.log(error);
-        file = null;
-    }
+	const { id } = context.query;
+	let file: IFile | null;
+	try {
+		const { data } = await axios.get<IFile>(
+			`${process.env.API_BASE_URL}/api/files/${id}`
+		);
+		file = data;
+	} catch (error) {
+		console.log(error);
+		file = null;
+	}
 
-    return {
-        props: {
-            file,
-        },
-    };
+	return {
+		props: {
+			file,
+		},
+	};
 };
